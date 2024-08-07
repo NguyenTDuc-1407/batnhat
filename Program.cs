8027d98cec8b9abe6730847d91491be55b78abd2
Duc.NT <311020ntd@gmail>
2024-08-07 19:30:59 +0700

    bat nhat

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+double solution;
+if (a == 0)
+{
+    if (b == 0)
+    {
+        Console.WriteLine("vo so nghiem");
+    }
+    else
+    {
+        Console.WriteLine("vo nghiem");
+    }
+}
+else
+{
+    if (b == 0)
+    {
+        Console.WriteLine("ket qua: 0");
+    }
+    else
+    {
+        solution = -b / a;
+        Console.WriteLine("ket qua: " + solution);
+    }
+
+}
